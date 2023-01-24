69eda027e9cc5e99c8ef90a1581f29058477c44c
tanohub <scecconello@gmail.com>
2023-01-24 23:42:51 +0100

    Added adminer container, proxysql configuration

--- provision/mysql/init/01-create_db.sql
+++ provision/mysql/init/01-create_db.sql
@@ -1,12 +1,13 @@
-# create databases
+-- proxysql monitoring user
+CREATE USER 'proxysql-monitor'@'%' IDENTIFIED BY 'Password123';
+GRANT USAGE ON *.* TO 'proxysql-monitor'@'%';
+
+-- create database and application user
 CREATE DATABASE IF NOT EXISTS `DB01`;
-
-# create DB01 user and grant rights
 CREATE USER 'db01user01'@'%' IDENTIFIED BY 'Password123';
-CREATE USER 'db01user02'@'%' IDENTIFIED BY 'Password123';
-GRANT ALL PRIVILEGES ON DB01.* TO 'db01user02'@'%';
-GRANT SELECT ON DB01.* TO 'db01user02'@'%';
+GRANT ALL PRIVILEGES ON DB01.* TO 'db01user01'@'%';
 
+-- insert some data on DB01.movies
 use DB01;
 CREATE TABLE movies(
 	title VARCHAR(50) NOT NULL,
@@ -16,4 +17,5 @@ CREATE TABLE movies(
 PRIMARY KEY(title));
 
 INSERT INTO movies VALUE ("Joker", "psychological thriller", "Todd Phillips", 2019);
-INSERT INTO movies VALUE ("Alien", "Sci Fi", "Dumbo", 1978);
+INSERT INTO movies VALUE ("Alien", "Sci Fi", "Ridley Scott", 1979);
+INSERT INTO movies VALUE ("Tremors", "Epic", "Ron Underwood", 1990);
